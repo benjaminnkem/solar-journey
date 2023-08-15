e3920f231f2e21141d47cdc4e0c415c1801827f2
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-08-15 22:44:07 +0100

    Independent practice

--- main.js
+++ main.js
@@ -9,16 +9,19 @@ const sizes = {
   height: window.innerHeight,
 };
 
-const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshStandardMaterial({ color: 0x00ff00, roughness: 50 });
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+const geometry = new THREE.SphereGeometry(5, 64, 64);
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00, roughness: 20 });
+const sun = new THREE.Mesh(geometry, material);
+scene.add(sun);
 
-const ambientLight = new THREE.AmbientLight(0x333333, 1);
-const light = new THREE.PointLight(0xffffff, 200);
-const lightHelper = new THREE.PointLightHelper(light)
-light.position.set(0, 10, 10);
-scene.add(ambientLight, light, lightHelper);
+const mainLight = new THREE.PointLight(0xffffff, 200);
+mainLight.position.set(8, 8, 8);
+
+const pointHelper = new THREE.PointLightHelper(mainLight);
+scene.add(mainLight, pointHelper);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.z = 10;
@@ -29,28 +32,26 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setPixelRatio(2);
 renderer.setSize(sizes.width, sizes.height);
 
-// Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.enablePan = false;
-controls.enableZoom = false;
-controls.autoRotate = true;
-controls.autoRotateSpeed = 5;
+controls.enableZoom = true;
 
 window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
 
-  // update camera
   camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
   renderer.setSize(sizes.width, sizes.height);
 });
 
-function animate() {
+const animate = () => {
   requestAnimationFrame(animate);
   controls.update();
+
+  sun.rotation.set(0, 10, 0)
   renderer.render(scene, camera);
-}
+};
 
 animate();
